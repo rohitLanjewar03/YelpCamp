4f4779641865b11d0f60842b44c98ac45be7a08b
rohitLanjewar03 <148527328+rohitLanjewar03@users.noreply.github.com>
2024-08-11 18:12:37 +0530

    Added Rviews, flash and restructured

--- schemas.js
+++ schemas.js
@@ -1,4 +1,5 @@
-const Joi = require('joi')
+const Joi = require('joi');
+const review = require('./models/review');
 
 module.exports.campgroundSchema = Joi.object({
     campground: Joi.object({
@@ -7,5 +8,12 @@ module.exports.campgroundSchema = Joi.object({
         image: Joi.string().required(),
         location: Joi.string().required(),
         description: Joi.string().required()
+    }).required()
+});
+
+module.exports.reviewSchema = Joi.object({
+    review: Joi.object({
+        rating: Joi.number().min(1).max(5).required(),
+        body: Joi.string().required()
     }).required()
 })
