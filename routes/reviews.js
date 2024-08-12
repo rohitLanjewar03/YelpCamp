e887b588ded34b2b5c21debba68c6642e4f45691
rohitLanjewar03 <148527328+rohitLanjewar03@users.noreply.github.com>
2024-08-12 19:16:00 +0530

    Added Basic authentication and authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -1,38 +1,25 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
-
 const Campground = require('../models/campground');
 const Review = require('../models/review');
-
 const { reviewSchema } = require('../schemas.js');
-
-
+const {validateReview,isLoggedIn,isReviewAuthor} = require('../middleware.js')
 const ExpressError = require('../utils/ExpressError');
 const catchAsync = require('../utils/catchAsync');
 
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
-    } else {
-        next();
-    }
-}
 
-
-
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', validateReview,isLoggedIn, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
+    review.author = req.user._id;
     await review.save();
     await campground.save();
     req.flash('success', 'Created new review!');
     res.redirect(`/campgrounds/${campground._id}`);
 }))
 
-router.delete('/:reviewId', catchAsync(async (req, res) => {
+router.delete('/:reviewId',isLoggedIn,isReviewAuthor,catchAsync(async (req, res) => {
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
--- seeds/index.js
+++ seeds/index.js
@@ -19,6 +19,7 @@ const seedDB = async()=>{
         const random1000 = Math.floor(Math.random()*1000);
         const price = Math.floor(Math.random()*1000);
         const camp = new Campground({
+            author: '66b8c817d86f930466bfd8e5',
             title:`${sample(descriptors)} ${sample(places)}`,
             location: `${cities[random1000].city},${cities[random1000].state}`,
             image: `https://picsum.photos/400?random=${Math.random()}`,
