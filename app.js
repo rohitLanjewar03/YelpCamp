39b30a66c487f435736b61ef212f56c7a36c1cf0
rohitLanjewar03 <148527328+rohitLanjewar03@users.noreply.github.com>
2024-08-21 21:30:18 +0530

    Added some security features

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-if(process.env.NODE_ENC != "production"){
-    require('dotenv').config();
+ if(process.env.NODE_ENC!= "production"){
+     require('dotenv').config();
 }
 
 const express = require('express');
@@ -13,6 +13,7 @@ const methodOverride = require('method-override');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const User = require('./models/user')
+const helmet = require('helmet');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
@@ -38,6 +39,7 @@ app.use(methodOverride('_method'));
 app.use(express.static(path.join(__dirname, 'public')))
     
 const sessionConfig = {
+    name:'blah',
     secret: 'thisshouldbeabettersecret!',
     resave: false,
     saveUninitialized: true,
@@ -47,8 +49,61 @@ const sessionConfig = {
         maxAge: 1000 * 60 * 60 * 24 * 7
     }
     }
+
+    const scriptSrcUrls = [
+        "https://stackpath.bootstrapcdn.com/",
+        // "https://api.tiles.mapbox.com/",
+        // "https://api.mapbox.com/",
+        "https://kit.fontawesome.com/",
+        "https://cdnjs.cloudflare.com/",
+        "https://cdn.jsdelivr.net",
+        "https://cdn.maptiler.com/", // add this
+    ];
+    const styleSrcUrls = [
+        "https://kit-free.fontawesome.com/",
+        "https://stackpath.bootstrapcdn.com/",
+        // "https://api.mapbox.com/",
+        // "https://api.tiles.mapbox.com/",
+        "https://fonts.googleapis.com/",
+        "https://use.fontawesome.com/",
+        "https://cdn.jsdelivr.net",
+        "https://cdn.maptiler.com/", // add this
+    ];
+    const connectSrcUrls = [
+        // "https://api.mapbox.com/",
+        // "https://a.tiles.mapbox.com/",
+        // "https://b.tiles.mapbox.com/",
+        // "https://events.mapbox.com/",
+        "https://api.maptiler.com/", // add this
+    ];
 app.use(session(sessionConfig))
 app.use(flash());
+const fontSrcUrls = [];
+app.use(
+    helmet.contentSecurityPolicy({
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'unsafe-inline'", "'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dk28k2e0q/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT! 
+                "https://images.unsplash.com/",
+                "https://api.maptiler.com/",
+            
+            ],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    })
+);
+
+
+
 
 app.use(passport.initialize());
 app.use(passport.session());
